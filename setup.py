707c4614b72b6dc9d4812724e768001149411938
Markus Dangl <663399+xicesky@users.noreply.github.com>
2023-06-06 13:36:17 +0200

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='minecraft-serverwrapper',
-    version='0.2',
+    version='0.3.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
