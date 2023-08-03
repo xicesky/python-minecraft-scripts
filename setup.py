05fbb8c187387aba202c2537030ae195cf5404db
Markus Dangl <663399+xicesky@users.noreply.github.com>
2023-08-03 22:51:23 +0200

    Bump version bc of maj changes
    also auto-update pre-commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='minecraft-serverwrapper',
-    version='0.4.2',
+    version='0.5.0',
     packages=find_packages(),
     package_data={'minecraft.serverwrapper': ['*.yaml']},
     include_package_data=True,
